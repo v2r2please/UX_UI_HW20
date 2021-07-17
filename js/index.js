b4256754dec0598e5bb5cd3008d3a9d3dc155e8b
v2r2please <76893867+v2r2please@users.noreply.github.com>
2021-07-16 18:12:30 -0700

    blah

--- js/index.js
+++ js/index.js
@@ -1,52 +1,25 @@
-var accordion = (function(){
-  
-    var $accordion = $('.js-accordion');
-    var $accordion_header = $accordion.find('.js-accordion-header');
-    var $accordion_item = $('.js-accordion-item');
-   
-    // default settings 
-    var settings = {
-      // animation speed
-      speed: 400,
-      
-      // close all other accordion items if true
-      oneOpen: false
-    };
-      
-    return {
-      // pass configurable object literal
-      init: function($settings) {
-        $accordion_header.on('click', function() {
-          accordion.toggle($(this));
-        });
-        
-        $.extend(settings, $settings); 
-        
-        // ensure only one accordion is active if oneOpen is true
-        if(settings.oneOpen && $('.js-accordion-item.active').length > 1) {
-          $('.js-accordion-item.active:not(:first)').removeClass('active');
-        }
-        
-        // reveal the active accordion bodies
-        $('.js-accordion-item.active').find('> .js-accordion-body').show();
-      },
-      toggle: function($this) {
-              
-        if(settings.oneOpen && $this[0] != $this.closest('.js-accordion').find('> .js-accordion-item.active > .js-accordion-header')[0]) {
-          $this.closest('.js-accordion')
-                 .find('> .js-accordion-item') 
-                 .removeClass('active')
-                 .find('.js-accordion-body')
-                 .slideUp()
-        }
-        
-        // show/hide the clicked accordion item
-        $this.closest('.js-accordion-item').toggleClass('active');
-        $this.next().stop().slideToggle(settings.speed);
-      }
+$(document).ready(function() {
+
+  $(".toggle-accordion").on("click", function() {
+    var accordionId = $(this).attr("accordion-id"),
+      numPanelOpen = $(accordionId + ' .collapse.in').length;
+    
+    $(this).toggleClass("active");
+
+    if (numPanelOpen == 0) {
+      openAllPanels(accordionId);
+    } else {
+      closeAllPanels(accordionId);
     }
-  })();
-  
-  $(document).ready(function(){
-    accordion.init({ speed: 300, oneOpen: true });
-  });
+  })
+
+  openAllPanels = function(aId) {
+    console.log("setAllPanelOpen");
+    $(aId + ' .panel-collapse:not(".in")').collapse('show');
+  }
+  closeAllPanels = function(aId) {
+    console.log("setAllPanelclose");
+    $(aId + ' .panel-collapse.in').collapse('hide');
+  }
+     
+});
